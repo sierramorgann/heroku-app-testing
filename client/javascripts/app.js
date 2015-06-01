fdd905a60a08f6bd155e1c75c192a7dc3144345f
Sierra Morgan <sierramorgan07@gmail.com>
2015-06-01 11:48:07 -0700

    work?

---
+++ client/javascripts/app.js
@@ -0,0 +1,31 @@
+var evntHandler = function() {
+
+    var prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+    var suffixURL = "&format=json&jsoncallback=?";
+    var flickrTag = $("input").val();
+    var requestURL = prefixURL + flickrTag + suffixURL;
+
+    //clear old photos
+    $(".photos").html("");
+
+    var displayImg = function(index) {
+
+        $.getJSON(requestURL, function(flickrResponse) {
+            var photoArr = flickrResponse.items;
+            
+            var $img = $("<img>").hide();
+            $img.attr('src', photoArr[index].media.m).hide();
+          	$(".photos").empty();
+            $("main .photos").append($img);
+            $img.fadeIn();
+
+            setTimeout(function() {
+                index = (index + 1) % 20;
+                displayImg(index);
+            }, 1000);
+        });
+    };
+    displayImg(0);
+};
+
+$("button").on("click", evntHandler);
---
+++ web.js
@@ -0,0 +1,29 @@
+var express = require("express"),
+    bodyParser = require("body-parser"),  //add this line
+    http = require("http"),
+    app = express();
+ 
+app.use(bodyParser.urlencoded()); 
+app.use(express.static(__dirname + "/client"));
+ 
+// Create our Express-powered HTTP server
+http.createServer(app).listen(3000);
+ 
+// set up our routes
+app.get("/hello", function (req, res) {
+    res.send("<h2>Hello World!</h2>");
+});
+ 
+app.get("/goodbye", function (req, res) {
+    res.send("<h2>Goodbye World!</h2>");
+});
+ 
+// set up the root route
+app.get("/", function (req, res) {
+    res.send("<h2>This is the root route!</h2>");
+});
+ 
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
